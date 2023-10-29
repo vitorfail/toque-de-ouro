0d57b3a0f059392f7b4d54ced9e3832a54da8aae
vitorfail <vitor_andrademanoel@hotmail.com>
2023-10-29 16:46:33 -0300

    tentando creiar compra

---
+++ src/app/Compra/page.js
@@ -0,0 +1,63 @@
+"use client";
+import Image from 'next/image'
+import { Header } from '../Header/Header'
+import Lista from "../image/list.png"
+import Logo from "../image/logo1.png"
+import li from '../menu';
+import "./page.css"
+// eslint-disable-next-line import/no-webpack-loader-syntax
+
+import {AiFillInstagram, AiFillYoutube, AiFillFacebook} from "react-icons/ai"
+import Seta from '../Seta';
+import { Rodape } from '../Rodape';
+
+export default function Home() {
+  return (
+    <div className='app'>
+      <Seta></Seta>
+      <Header></Header>
+      <div className='cabecalho'>
+        <Image style={{width:"20px", height:"20px"}} src={Lista}></Image>
+        <Image style={{width:"40px", height:"40px"}} src={Logo}></Image>
+      </div>
+      <div className="header compra">
+            <div className="logo">
+                <div className="img"></div>
+            </div>
+            <div className="menu">
+              {li.map((item, key) => (
+                <div key={key} className="item">
+                  <div className="titulo">
+                    <p>{item}</p>
+                    <Image src={Seta} height={15} width={15}></Image>
+                  </div>
+                  <span className="linha"></span>
+                </div>
+              ))}
+            </div>
+      </div>
+            <div className="aviso">
+              Compra agora!
+            </div>
+      <section className='info'>
+        <div className='whats'>
+          <p> (88) 9878-9999</p>
+          <p>Da segunda ao sábado, das 8:00 as 17:30</p>
+        </div>
+        <div className="icons">
+              <a href="https://www.instagram.com/vitor_andrademanoel04/" target="_blank">
+                  <AiFillFacebook color="white"></AiFillFacebook>
+              </a>
+              <a href="https://www.linkedin.com/in/vitor-manoel-programer/" target="_blank">
+                  <AiFillYoutube color="white"></AiFillYoutube>
+              </a>
+              <a href="https://github.com/vitorfail" target="_blank">
+                  <AiFillInstagram color="white"></AiFillInstagram>
+              </a>
+        </div>
+      </section>
+      <div className='quebra'></div>
+      <Rodape></Rodape>
+    </div>
+  )
+}
--- src/app/Rodape/index.js
+++ src/app/Rodape/index.js
@@ -4,40 +4,49 @@ import "./index.css"
 import Mail from "../image/mail.png"
 import Clock from "../image/clock.png"
 import Phone from "../image/phone.png"
+import {AiFillInstagram, AiFillYoutube, AiFillFacebook} from "react-icons/ai"
 export function Rodape(){
     return(
       <div className="rodape">
         <div className="inf">
           <div className="colum">
-            <p>Compania</p>
+            <p className="titulo">Compania</p>
             <p>Sobre nós</p>
             <p>Nos contacte</p>
           </div>
           <div className="colum">
-            <p>Informações</p>
+            <p className="titulo">Informações</p>
             <p>Endereço</p>
             <p>horarios</p>
           </div>
-          <div className="contato">
-            <div className="linha">
+          <div className="colum">
+            <div  className="titulo" id="linha">
               <Image alt="Phone" src={Phone} width={20} height={20}></Image>
               <p>Phone: (88) 981393182</p>
             </div>
-            <div className="linha">
+            <div id="linha">
               <Image alt="horario" src={Clock} width={20} height={20}></Image>
               <div>
                 <p style={{ marginBottom:"10px"}}>Segunda-Sexta: 8:00 - 17:30</p>
                 <p style={{ marginBottom:"10px"}}>Sab: 8:00 - 13:00</p>
               </div>
             </div>
-            <div className="linha">
+            <div id="linha">
               <Image alt="Phone" src={Mail} width={20} height={20}></Image>
               <p>ENVIE UM EMAIL</p>
             </div>
           </div>
-          <div className="contato">
+          <div className="colum">
+            <p className="titulo">Nos siga</p>
+            <div id="linha">
+              <AiFillFacebook style={{marginRight:"10px"}}></AiFillFacebook>
+              <p>FACEBOOK</p>
+            </div>
+            <div id="linha">
+              <AiFillInstagram style={{marginRight:"10px"}}></AiFillInstagram>
+              <p>INSTAGRAM</p>
+            </div>
           </div>
-
         </div>
         <p className="copy">Copyright @ 2023 Vitor Manoel</p>
       </div>
