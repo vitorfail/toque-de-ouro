62440cb76dd7723e4fd79f8efbd9c040d4f5cf18
Vitor Manoel de andrade <95591747+vitorfail@users.noreply.github.com>
2023-11-29 11:25:45 -0300

    Update index.js

--- src/app/Loading/index.js
+++ src/app/Loading/index.js
@@ -2,29 +2,6 @@ import "./index.css"
 
 export default function Loading(){
     return(
-    <div class="loading">
-        <div class="finger finger-1">
-          <div class="finger-item">
-            <span></span><i></i>
-          </div>
-        </div>
-        <div class="finger finger-2">
-          <div class="finger-item">
-            <span></span><i></i>
-          </div>
-        </div>
-        <div class="finger finger-3">
-            <div class="finger-item">
-                <span></span><i></i>
-            </div>
-        </div>
-        <div class="finger finger-4">
-            <div class="finger-item">
-                <span></span><i></i>
-            </div>
-        </div>
-        <div class="last-finger">
-            <div class="last-finger-item"><i></i></div>
-        </div>      
-    </div>)
+        <div class="word">CARREGANDO...</div>
+<div class="overlay"></div>)
 }
